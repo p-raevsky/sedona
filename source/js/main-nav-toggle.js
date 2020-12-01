ba558ee77702faa7bd0df3cb1de84ced20df5ca3
p-raevsky <raevsky3@gmail.com>
2020-12-01 22:19:55 +0200

    Внес правки: поправил усы и клас в кнопке закрыть

--- source/js/main-nav-toggle.js
+++ source/js/main-nav-toggle.js
@@ -1,20 +1,20 @@
 var navMain = document.querySelector('.main-nav__list');
-var navToggle = document.querySelector('.main-nav__toggle');
-var noJs = document.querySelector('.main-nav__toggle');
+var navToggle = document.querySelector('.page-header__toggle');
+var noJs = document.querySelector('.page-header__toggle');
 
-if (noJs.classList.contains('main-nav__toggle--nojs')) {
+if (noJs.classList.contains('page-header__toggle--nojs')) {
   navMain.classList.add('main-nav__list--closed');
-  navToggle.classList.add('main-nav__toggle--closed');
+  navToggle.classList.add('page-header__toggle--closed');
 }
 
-navToggle.classList.remove('main-nav__toggle--nojs');
+navToggle.classList.remove('page-header__toggle--nojs');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav__list--closed')) {
     navMain.classList.remove('main-nav__list--closed');
-    navToggle.classList.remove('main-nav__toggle--closed');
+    navToggle.classList.remove('page-header__toggle--closed');
   } else {
     navMain.classList.add('main-nav__list--closed');
-    navToggle.classList.add('main-nav__toggle--closed');
+    navToggle.classList.add('page-header__toggle--closed');
   }
 });
